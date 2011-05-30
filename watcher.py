9f8006f7155d42761209cf1ef72f9c95dcc71e2e
Steve Gattuso <stevenleeg@gmail.com>
2011-05-30 18:45:06 -0400

    Implementing screenshot watching - Implements the ability to identify new screenshots in the watch path, set in macgrab.conf

--- watcher.py
+++ watcher.py
@@ -4,13 +4,28 @@
 # http://www.stevegattuso.me // @vestonian
 # Distributed under the MIT license
 #
-import os, time, re
+import os, time, re, logging
+import macgrab
+
+# Get the config file
+config = macgrab.getConfig()
+watch_path = config.get("general","watch_path")
+
+# Compile the regex used to identify screenshots
+screenshot_regex = re.compile("Screen shot ([0-9]{4})-([0-9]{2})-([0-9]{2}) at (1?[0-9]).([0-9]{2}).([0-9]{2}) ([A|PM]{2}).png")	
 
 while 1:
-	# Get the path to their desktop and list it
-	path = os.path.join(os.path.expanduser("~"), "Desktop")
-	watch_dir = os.listdir(path)
+	# Get a list of filenames in the watch directory
+	files = os.listdir(watch_path)
 
+	screenshots = []
 	# See if there are any screenshots to upload
-	for filename in watch_dir:
-		regex = re.compile("Screen shot ([0-9]{4})-([0-9]{2})-([0-9]{2}) at (1?[0-9]).([0-9]{2}).([0-9]{2}) ([A|PM]{2}).png")	
+	for filename in files:
+		if screenshot_regex.match(filename):
+			screenshots.append(filename)
+			
+	if len(screenshots) > 0:
+		logging.info("Found screenshots to upload: %s" % screenshots)
+	
+	# Sleep for a bit, since we don't need to be doing this all the time.
+	time.sleep(1)
